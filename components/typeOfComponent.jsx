f7819044d865dcca23131b42ac8064a9363575c4
Muhammad Wafa <wafax.4@gmail.com>
2020-06-29 18:38:31 +0700

    format code

--- components/typeOfComponent.jsx
+++ components/typeOfComponent.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export const FuncComponent = () => <h1>This is a Functional Component</h1>
 
 export class ClasComponent extends Component {
-    render() {
-        return <h1>This is a Class Component</h1>
-    }
+  render() {
+    return <h1>This is a Class Component</h1>
+  }
 }
