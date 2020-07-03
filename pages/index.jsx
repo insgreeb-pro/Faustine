424aa91a5eeed1f84dc7fac90d07a80e88a39d41
FaustineKachina <faustinekachina@gmail.com>
2020-07-03 16:05:46 +0700

    menambahkan file properti

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,24 @@
+import Head from 'next/head'
 import { Container } from '../components/Container'
 import { FuncComponent, ClasComponent } from '../components/typeOfComponent'
+import { Cekproperti } from '../components/assignproperty'
 
 export default () => {
   return (
     <div>
-      <FuncComponent />
-      <ClasComponent />
+      <Head>
+        <title>skripsi</title>
+      </Head>
       <Container title="sedboi.png">
-        <h1>meong</h1>
-        <div>minko</div>
+        <FuncComponent />
+        <ClasComponent />
+      </Container>
+      <Container>
+        <Cekproperti nama="Asep" gelar="pengamen">
+          <h4>this my children</h4>
+        </Cekproperti>
+        <Cekproperti nama="Jajang" gelar="pengusaha" />
+        <p>Jajajajaja</p>
       </Container>
     </div>
   )
